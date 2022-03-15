9aa6f192fb640eb5ab6abadf48aed53b426dc600
Vladislav Ishchenko <boshik64new@gmail.com>
2022-03-15 17:25:02 +0300

    1.0

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,44 @@
-import Message from "./components/Message";
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
+import Message from "./Message";
+import "./message.css";
 
 function App() {
-  const text = 'Какой-то текст описанный в константе, который мы прокидываем через пропсы в компоненту \'Message\' и стилизуем. DevTools так-же установлен в chrome'
-  const name = 'гость'
+  const [messages, setMessage] = useState([]);
+
+  const createMessage = (newMessage) => {
+    setMessage([...messages, newMessage]);
+  };
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      let lastMsg = messages[messages.length - 1];
+      let robotMsg = {
+        id: Date.now(),
+        author: "Робот",
+        textMessage:
+          "Здравствуйте, " +
+          lastMsg.author +
+          ". Это сообщение отправлено роботом.",
+      };
+      if (lastMsg.author !== "Робот") {
+        setTimeout(() => {
+          setMessage([...messages, robotMsg]);
+        }, 1500);
+      }
+    }
+  }, [messages]);
+
   return (
     <div className="App">
-      <p>Привет, {name}</p>
-      <Message text={text} />
-
+      <header>
+        <div className="App-header">
+          {messages.map((message) => (
+            <Message message={message} key={message.id} />
+          ))}
+        </div>
+      </header>
+      <Form create={createMessage} />
     </div>
   );
 }
